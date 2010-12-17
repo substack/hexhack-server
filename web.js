32d4cceccfe1377180b21a23ffa040ced2ba3b6a
James Halliday <substack@gmail.com>
2010-12-17 12:21:22 +0000

    moar deps

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = module.exports = express.createServer();
 
+app.use(express.staticProvider(__dirname + '/static'));
+
 app.get('/dnode.js', require('dnode/web').route());
 
 app.get('/', function (req, res) {
